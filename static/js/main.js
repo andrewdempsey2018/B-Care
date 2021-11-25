bf988cb632cb098c8412e92ae0b66d988ac43b09
marinaP <mar.pavlovic@gmail.com>
2021-11-25 16:12:16 +0100

    Add back to top arrow button

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,23 @@
+// Back to Top Arrow - code with modification sourced from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+mybutton = document.getElementById('arrow_2top');
+
+// When the user scrolls down 250px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250|| document.documentElement.scrollTop > 250) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the page
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
 console.log("sanity test");
