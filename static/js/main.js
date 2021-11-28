e5720893484199c09c45105c1cc3162dfa65f7e6
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2021-11-28 21:01:06 +0000

    modals added to stories page

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,10 @@
 mybutton = document.getElementById('arrow_2top');
 
 // When the user scrolls down 250px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 250|| document.documentElement.scrollTop > 250) {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
     mybutton.style.display = 'block';
   } else {
     mybutton.style.display = 'none';
@@ -18,6 +18,43 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+// ----------- Modals
 
+// Get the modal
+const modal = document.getElementById("modal");
 
-console.log("sanity test");
+// Get the button that opens the modal
+const delete_buttons = document.getElementsByClassName("delete_button");
+const cancel_button = document.getElementById("cancel_button");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+let modal_target = "";
+
+// When the user clicks on the button, open the modal
+Array.from(delete_buttons).forEach(button => {
+  button.onclick = () => {
+    modal_target = button.getAttribute('data-story');
+    document.getElementById('modal_delete_button').setAttribute('href', "delete_story?" + "storyId=" + modal_target);
+    console.log(modal_target);
+      modal.style.display = "block";
+  }
+})
+
+cancel_button.onclick = () => {
+  modal.style.display = "none";
+}
+
+span.onclick = () => {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// ----------- Modals
